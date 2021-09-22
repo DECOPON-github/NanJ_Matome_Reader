346cb220241ed629d346084c9b3843b881e8d872
decopon <decopon.labs@gmail.com>
2021-09-22 13:49:44 +0900

    Initial commit

---
+++ app/src/main/java/com/decopon/nanj_matome_reader/MainActivity.java
@@ -0,0 +1,14 @@
+package com.decopon.nanj_matome_reader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
